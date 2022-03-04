edb9e068f5cb302a63f475772d2c2c9846ed7533
Max-09 <81917083+Max-09@users.noreply.github.com>
2022-03-04 18:49:36 -0300

    Barra de busqueda
    Ahora podes buscar tus pokemónes si conoces su nombre

--- src/getPokemon.js
+++ src/getPokemon.js
@@ -10,28 +10,29 @@ function createPokemon(pokemon){
     const $pokeContainer = document.querySelector('#container');
 
     const $pokeName = document.createElement('h1');
-    const $pokeimg = document.createElement('img');
-    const $poketype = document.createElement('p');
+    const $pokeImg = document.createElement('img');
+    const $pokeType = document.createElement('p');
 
     $pokeName.innerHTML = pokemon.name;
-    $pokeimg.src = pokemon.sprites['front_default'];
+    $pokeImg.src = pokemon.sprites['front_default'];
+    const $type = pokemon.types.map((type) => type.type.name).join(', ');
+    $pokeType.innerHTML = $type;
 
     $pokeDiv.classList.add("rojo");
-    $pokeDiv.appendChild($pokeName);
-    $pokeDiv.appendChild($pokeimg);
+    $pokeDiv.id = pokemon.name;
+    $pokeDiv.append($pokeName, $pokeImg, $pokeType);
     $pokeContainer.appendChild($pokeDiv);
-
 }
 
 const generations = {
-    1: [1, 151],
-    2: [152, 251],
-    3: [252, 386],
-    4: [387, 493],
-    5: [494, 649],
-    6: [650, 721],
-    7: [722, 809],
-    8: [810, 905]
+    "1": [1, 151],
+    "2": [152, 251],
+    "3": [252, 386],
+    "4": [387, 493],
+    "5": [494, 649],
+    "6": [650, 721],
+    "7": [722, 809],
+    "8": [810, 905]
 }
 
 function buttonsGeneration(num){
@@ -39,20 +40,51 @@ function buttonsGeneration(num){
 
     for(let i=1; i<=num; i++){
         const $button = document.createElement('button');
-        $button.value = i;
+        $button.id = i;
         $button.textContent = `GENERATION ${i}`;
+        const setGeneration = generations[i];
+        $button.onclick = function(){
+            getPokemons(setGeneration);
+        }
         $generation.appendChild($button);
     }
 }
 
-async function getPokemons(id){
-    for(let i=1; i<=id; i++){
-        getPokemon(i);
+async function getPokemons(gen){
+    const $container = document.querySelector('#container');
+    $container.innerHTML = '';
+    for(let i=gen[0]; i<=gen[1]; i++){
+       pokeApi(i);
+    }
+}
+
+//SEARCHING
+const pokemo = document.getElementById('searchPoke');
+pokemo.addEventListener('input', searchPokemon);
+
+async function searchPokemon(){
+    const $pokeName = document.querySelector('input').value.toLowerCase();
+    const $searchContainer = document.querySelector('#searchResult');
+    const $pokemonFound = document.getElementById(`${$pokeName}`);
+    const $container = document.querySelector('#container');
+
+    if(document.getElementById(`${$pokeName}`)){
+        $searchContainer.appendChild($pokemonFound);
+        $container.classList.add("hide");
+    }else{
+        $searchContainer.innerHTML = '';
+        $container.classList.remove("hide");
     }
     
 }
 
+
 buttonsGeneration(8);
+getPokemons(generations["1"]);
+
+
+
+
 
 
 
